9af4b93a5d7e60d063887380cddd994048caf477
jihyeM <linettem@naver.com>
2016-11-30 23:38:14 +0900

    Statement class

---
+++ src/Statement.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Statement {
+	public String addStatement(int state){
+		return "add";
+	}
+	public String viewStatement(int state){
+		return "view";
+	}
+	public String deleteStatement(int state){
+		return "delete";
+	}
+}
