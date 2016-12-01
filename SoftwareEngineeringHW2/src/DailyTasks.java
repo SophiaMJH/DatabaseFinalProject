32eb892d0bd4ba02f156d908d1d6362e4bb8b830
ye0013 <ye0013@gmail.com>
2016-12-02 00:12:40 +0900

    Statement

--- SoftwareEngineeringHW2/src/DailyTasks.java
+++ SoftwareEngineeringHW2/src/DailyTasks.java
@@ -2,23 +2,37 @@ import java.util.*;
 import java.io.*;
 
 
+class PhoneBook{
+	String id;
+	String name;
+	String phoneNumber;
+	int phoneIndex;
+}
+
+class Schedule{
+	String id;
+	Date date;
+	String description;
+	int scheduleIndex;
+}
+
+class Note{
+	String id;
+	String note;
+	int noteIndex;
+}
+
+
+
 class Statement{
 	
 	final int PHONEBOOK = 1;
 	final int SCHEDULE = 2;
 	final int NOTE = 3;
-	
 	private String sqlStatement;
 	private String databaseName;
 	private String databaseAttributes;
-	private String valuesForAddition;
-	private int index;
-	private String id;
-	
-	private void setIndex(int maxIndex){
-		index = maxIndex;	// add 할 때의 index 넣는다.
-	}
-	
+
 	private void setDatabaseName(int state){
 		if(state == PHONEBOOK)
 			databaseName = "PhoneBook";
@@ -27,7 +41,7 @@ class Statement{
 		else if(state == NOTE)
 			databaseName = "Note";
 		else
-			;//Exeption
+			;//Exception
 	}
 	
 	private void setDatabaseAttributesForAddition(int state){
@@ -38,60 +52,169 @@ class Statement{
 		else if(state == NOTE)
 			databaseAttributes = "(id, note, noteIndex ) ";
 		else
-			;//Exeption
+			;//Exception
 	}
 	
-	private void setValuesForAddition(int state){
+	private String makeValueStringForAddition(int state, int maxIndex){
 		
 		Scanner scan = new Scanner(System.in);
+		String valuesForAddition = "";
+		InputFromUser inputFromUser = new InputFromUser();
 		if(state == PHONEBOOK){
-			String name = "";
-			String phoneNumber = "";
-			
-			System.out.print("name : ");
-			name = scan.next();
-			System.out.print("phone : ");
-			phoneNumber = scan.next();
-			valuesForAddition = "values("+id+", "+name+", "+phoneNumber+", "+index+")";
+			PhoneBook valuesForPhoneBook = inputFromUser.queryAndSetPhoneBook(maxIndex);
+			valuesForAddition = "values("
+								+ valuesForPhoneBook.id + ", "
+								+ valuesForPhoneBook.name +", "
+								+ valuesForPhoneBook.phoneNumber + ", "
+								+ valuesForPhoneBook.phoneIndex + ")";
 		}
 		else if(state == SCHEDULE){
-			Date date;
-			int year, month, day;
-			String description = "";
-			
-			System.out.print("date : ");
-			year = scan.nextInt();
-			month = scan.nextInt();
-			day = scan.nextInt();
-			date = new Date(year, month, day);
-			
-			System.out.print("description : ");
-			description = scan.nextLine();
-			valuesForAddition = "values("+id+", "+date+", "+description+", "+index+") ";
+			Schedule valuesForSchedule = inputFromUser.queryAndSetSchedule(maxIndex);
+			valuesForAddition = "values("
+								+ valuesForSchedule.id + ", "
+								+ valuesForSchedule.date + ", "
+								+ valuesForSchedule.description + ", "
+								+ valuesForSchedule.scheduleIndex + ") ";
 		}
 		else if(state == NOTE){
-			String note = "";
-			
-			System.out.print("["+index+"] note : " );
-			note = scan.nextLine();
-			valuesForAddition = "values("+id+", "+note+", "+index+")";;
+			Note valuesForNote = inputFromUser.queryAndSetNote(maxIndex);
+			valuesForAddition = "values(" 
+								+ valuesForNote.id + ", "
+								+ valuesForNote.note + ", "
+								+ valuesForNote.noteIndex + ")";
 		}
 		else{
 			//exception
 		}
+		return valuesForAddition;
 	}
 	
-	private String makeStatementForAddition(int state, int index){
+	private String makeIndexName(int state){
+		String indexName = "";
+		if(state == PHONEBOOK)
+			indexName = "phoneIndex";
+		else if(state == SCHEDULE)
+			indexName = "scheduleIndex";
+		else if(state == NOTE)
+			indexName = "noteIndex";
+		else
+			;
+		return indexName;
+	}
+
+	private String makeStatementForAddition(int state, int maxIndex){
 		
-		setIndex(index);
 		setDatabaseName(state);
 		setDatabaseAttributesForAddition(state);
-		setValuesForAddition(state);
-		sqlStatement = "INSERT INTO " + databaseName + databaseAttributes + valuesForAddition;
+		sqlStatement = "INSERT INTO " 
+						+ databaseName
+						+ databaseAttributes
+						+ makeValueStringForAddition(state, maxIndex);;
+		return sqlStatement;
+	}
+	
+	private String makeStatementforViewing(int state){
+		
+		setDatabaseName(state);
+		sqlStatement = "SELECT * FROM " + databaseName;
+		return sqlStatement;
+	}
+	
+	private String deleteStatementforViewing(int state, int selectedIndex){
+		
+		setDatabaseName(state);
+		sqlStatement = "DELETE FROM "+ databaseName + "WHERE " + makeIndexName(state) + "=" + selectedIndex;
 		return sqlStatement;
 	}
 	
 }
+
+
+
+
+
+class InputFromUser{
+	
+	final int PHONEBOOK = 1;
+	final int SCHEDULE = 2;
+	final int NOTE = 3;
+	
+	public PhoneBook queryAndSetPhoneBook(int index){
+		
+		Scanner scan = new Scanner(System.in);
+		PhoneBook phoneBookForAddition = new PhoneBook();
+		///id
+		System.out.print("name : ");
+		phoneBookForAddition.name = scan.nextLine();
+		System.out.print("phone : ");
+		phoneBookForAddition.phoneNumber = scan.nextLine();
+		phoneBookForAddition.phoneIndex = index;
+		return phoneBookForAddition;
+	}
+	
+	public Schedule queryAndSetSchedule(int index){
+		
+		Scanner scan = new Scanner(System.in);
+		Schedule scheduleForAddition = new Schedule();
+		///id
+		int year, month, day;
+		System.out.print("date : ");
+		year = scan.nextInt();
+		month = scan.nextInt();
+		day = scan.nextInt();
+		scheduleForAddition.date = new Date(year, month, day);
+		System.out.print("description : ");
+		scheduleForAddition.description = scan.nextLine();
+		scheduleForAddition.scheduleIndex = index;
+		return scheduleForAddition;
+	}
+	
+	public Note queryAndSetNote(int index){
+		
+		Scanner scan = new Scanner(System.in);
+		Note noteForAddition = new Note();
+		//id
+		System.out.print("["+index+"] note : " );
+		noteForAddition.note = scan.nextLine();
+		noteForAddition.noteIndex = index;
+		return noteForAddition;
+	}
+	
+	private void queryForindexNumber(int state){
+		
+		if(state == PHONEBOOK){
+			System.out.print("지우고 싶은 사람의 인덱스를 입력하세요: ");
+		}
+		else if(state == SCHEDULE){
+			System.out.print("지우길 원하는 스케줄 인덱스를 입력해 주세요: ");
+		}
+		else if(state == NOTE){
+			System.out.print("지우길 원하는 노트인덱스를 입력해 주세요 : ");
+		}
+		else{
+			// Exception
+		}
+	}
+	
+	private void reQueryForindexNumber(int state){
+		
+		if(state == PHONEBOOK){
+			System.out.print("번호 인덱스를 다시 입력해주세요 : ");
+		}
+		else if(state == SCHEDULE){
+			System.out.print("스케줄 인덱스를 다시 입력해 주세요 : ");
+		}
+		else if(state == NOTE){
+			System.out.print("노트인덱스를 다시 입력해 주세요 : ");
+		}
+		else{
+			// Exception
+		}
+	}
+}
+
+
+
 
 
 
