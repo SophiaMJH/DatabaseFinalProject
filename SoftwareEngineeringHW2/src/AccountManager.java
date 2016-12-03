4e0564557f87c2658a16bb5d5bc40b24acea13a6
dalcomchoco <ibuny0530@gmail.com>
2016-12-03 23:47:06 +0900

    AccountManager

---
+++ SoftwareEngineeringHW2/src/AccountManager.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+
+class Account{
+	String id;
+	String pw;
+	
+}
+
+
+public class AccountManager {
+
+
+	private int login(string id, string pw){
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("ID : ");
+		id = scan.nextLine();
+		System.out.print("PW : ");
+		pw = scan.nextLine();	
+		
+		//DB¶ûºñ±³
+		
+	}
+	
+	private void changeAccount(void){
+		
+		System.out.print("ID : ");
+		id = scan.nextLine();
+		System.our.print("PW : ");
+		pw = scan.nextLine();
+		
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+}
