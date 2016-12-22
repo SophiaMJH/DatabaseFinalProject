721c32b06067952c9d64619710214ebfcb346f61
OHjii <orora5411@naver.com>
2016-12-23 05:42:02 +0900

    Junit t0t

--- SoftwareEngineeringHW2/src/MenuTest.java
+++ SoftwareEngineeringHW2/src/MenuTest.java
@@ -9,13 +9,6 @@ public class MenuTest {
 		Menu menu = new Menu();
 		menu.showMainMenu();
 		assertEquals(menu.subMenu,0);
+		}
 	}
-
-}
-	/*@Test
-	public void queryBackMenuTest() throws Exception {
-		Menu menu = new Menu();
-		menu.queryBackMenu();
-	}
-}
-*/
+	
