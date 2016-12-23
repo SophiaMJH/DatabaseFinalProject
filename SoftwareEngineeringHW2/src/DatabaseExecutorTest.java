1a00164dbcc8097fdaa73a064a7460561d51f17e
jihyeM <linettem@naver.com>
2016-12-23 11:09:54 +0900

    final!

--- SoftwareEngineeringHW2/src/DatabaseExecutorTest.java
+++ SoftwareEngineeringHW2/src/DatabaseExecutorTest.java
@@ -11,4 +11,26 @@ public class DatabaseExecutorTest {
 		int SCHEDULE = 3;
 		assertNotNull(executedb.setIndexName(SCHEDULE));
 		}
+<<<<<<< HEAD
+	
+	private String setIndexName(int mainMenu){
+		final int PHONEBOOK = 2;
+		final int SCHEDULE = 3;
+		final int NOTE = 4;
+		
+		String indexName = "";
+		if(mainMenu == PHONEBOOK){
+			indexName = "phoneIndex";
+		}
+		else if(mainMenu == SCHEDULE){
+			indexName = "scheduleIndex";
+		}
+		else if(mainMenu == NOTE){
+			indexName = "noteIndex";
+		}
+		return indexName;
+	
+	}
+=======
+>>>>>>> 4b4cf47255e9bf83bc27f77b520048ccf51eb44e
 	}
